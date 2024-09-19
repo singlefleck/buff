c87cd35d830172156b4133d2c9c3539b39aaa1f0
srujan <srujan.reddy@grnconnect.com>
2024-09-20 00:09:17 +0530

    Add basic project live view

---
+++ lib/buff_web/controllers/buff_live.ex
@@ -0,0 +1,14 @@
+defmodule BuffWeb.BuffLive do
+  use Phoenix.LiveView
+
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Welcome to My Buff Home Page!</h1>
+    """
+  end
+end
